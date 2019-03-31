bf0434ffa0d46d760fba59e55d58de31ac0b06f9
Kamolwan Sahapornaudomkarn <kamolwan.june@gmail.com>
2019-03-31 13:15:46 +0700

    workshop3

--- angularproject/src/app/app.component.ts
+++ angularproject/src/app/app.component.ts
@@ -1,55 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonServiceService } from './common-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  name : string = 'June';
-  score : number = 49;
-
-  student:any = {
-    name : "June",
-    studentID : "5921602485",
-    weight : 55,
-    height : 175,
-  }
-  
-  studentList:any = [{
-    name : "Pang",
-    studentID : "5921602612",
-    weight : 40,
-    height : 150,
-  },
-  {
-    name : "Mo",
-    studentID : "5921602779",
-    weight : 54,
-    height : 159,
-    
-  },
-  {
-    name : "June",
-    studentID : "5921602485",
-    weight : 55,
-    height : 175,
-  }
-];
-
-
-  constructor(){
-    this.student.bmi=(this.student.weight/
-    ((this.student.height/100)*(this.student.height/100))).toFixed(2);
-  
-    this.studentList.map((object,index)=>{
-      let obj:any = object;
-      obj.bmi = (object.weight/((object.height/100)*(object.height/100))).toFixed(2);
-      return obj;
-   })
-  
-    console.log(this.studentList);
-
+result:any
+status:any
+request ={
+  key1:"",
+  key2:""
 }
 
+constructor(private service: CommonServiceService){
+    this.getData();
+
+}
+getData(){
+  this.service.getData(this.request).subscribe((Response:any) => {
+    console.log(Response);
+   this.result = Response.sum;  
+   this.status = Response.status;
+  });
+}
 }
