d0a32249b8882d3595fc535d34193d719d367e7a
Kamolwan Sahapornaudomkarn <kamolwan.june@gmail.com>
2019-03-31 16:50:54 +0700

    workshop3

--- angularproject/src/app/app.component.ts
+++ angularproject/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonServiceService } from './common-service.service';
+import { SelectorContext } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -14,9 +15,15 @@ request ={
   key1:"",
   key2:""
 }
+userDetail : any = {
+  id:"",
+  name:""
+}
+
 
 constructor(private service: CommonServiceService){
-    this.getData();
+   // this.getData();
+    this.getUserDetail();
 
 }
 getData(){
@@ -26,4 +33,56 @@ getData(){
    this.status = Response.status;
   });
 }
+
+noteDetail : any = [];
+studentId = "5721602287";
+getUserDetail(){
+  let request ={
+    id:this.studentId
+  }
+  this.service.getUserDetail(request).subscribe((response:any) =>{
+    console.log(response);
+    this.userDetail = response.user[0]
+    this.noteDetail = response.noteDetail[0].note_type
+    console.log(this.userDetail,this.noteDetail);
+  });
+
+}
+  
+  listDetail:any = [];
+  selectNote(list){
+    this.listDetail=list
+  }
+
+  errorMsg="";
+  updateNoteName(){
+    let request={
+      id:this.userDetail.id,
+      noteId:this.editNoteDetail.note_id,
+      name:this.editNoteDetail.note_name
+    }
+    this.service.updateNoteName(request).subscribe((Response:any)=>{
+      if(Response.success){
+        this.errorMsg="แก้ไขสำเร็จ"
+        this.getUserDetail();
+      }else{
+        this.errorMsg="แก้ไขไม่สำเร็จ"
+      }
+      setTimeout(()=>{
+        this.errorMsg=''
+      },2000)
+    });
+  }
+  
+  editNoteDetail : any = {
+    note_id:"",
+    name:""
+  };
+
+  editNote(note){
+    console.log(note);
+    this.editNoteDetail=JSON.parse(
+      JSON.stringify(note)
+    );
+  }
 }
--- angularproject/src/app/common-service.service.ts
+++ angularproject/src/app/common-service.service.ts
@@ -8,20 +8,32 @@ import { TransferHttpService } from '@gorniv/ngx-transfer-http';
 export class CommonServiceService {
   private port = '3001'
   private rootPath = 'http://localhost:'+this.port;
-  private getDataPath = this.rootPath +'/test/getData'
+  private getDataPath = this.rootPath +'/test/getData';
+  private postDataPath= this.rootPath +'/test/postData';
+
+  private getUserDetailPath= this.rootPath +'/user/getDetail';
+
+  private updateNotePath = this.rootPath +'/user/updateNoteName';
+
   parameter: any ={
     params:{},
     reponseType:"json"
   }
   constructor(
     private http: TransferHttpService,
-  ){
-    
+  ){}
+
+  updateNoteName(body){
+    return this.http.post(this.updateNotePath,body);
   }
 
   getData(param){
     this.parameter.params = param;
     return this.http.get(this.getDataPath,this.parameter)
+  }
+  getUserDetail(param){
+    this.parameter.params = param;
+    return this.http.get(this.getUserDetailPath,this.parameter)
   }
 }
 
